71eae3935149938def3c05083db668d12a368be0
Anthony Pasquariello <antpas@bu.edu>
2018-06-26 22:18:51 -0400

    Create anchor.h

---
+++ anchor.h
@@ -0,0 +1,14 @@
+#ifndef ANCHOR_H_ 
+#define ANCHOR_H_
+
+    void setupAnchor();
+    void pairWithTag(float x, float y);
+    void send_int_VLC(int sendInt, int feq);
+    void send_int_IR(int sendInt, int feq);
+    void send_char_VLC(char * sendCharArr, int feq);
+    void send_char_IR(char * sendInt, int feq);
+    void runAnchor();
+	
+#endif 
+
+
